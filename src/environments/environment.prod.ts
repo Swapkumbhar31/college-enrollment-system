3339feeca08cf4a0ecb63c48a9b3c9e82ff277ce
Swapnil Kumbhar <swapkumbhar31@gmail.com>
2023-02-06 16:01:13 -0500

    initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'class-enrollment-system',
+    appId: '1:1012456175214:web:e5db35ef1e6d3b9f1fd87f',
+    storageBucket: 'class-enrollment-system.appspot.com',
+    apiKey: 'AIzaSyBIQx5nq36NOs2qlp_z6X89rtE8br5p7is',
+    authDomain: 'class-enrollment-system.firebaseapp.com',
+    messagingSenderId: '1012456175214',
+  },
   production: true
 };
